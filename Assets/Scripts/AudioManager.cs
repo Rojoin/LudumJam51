52b8dbbea5c93abd20b3e1410f1032d78ec6f49b
Nicodragon25 <nicolasariellaure@gmail.com>
2022-10-04 00:52:28 -0300

    AudioMixer

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,24 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
-    static AudioManager audioManager;
+    public static AudioManager audioManager;
 
     [SerializeField] private AudioClip[] audios;
-
     [SerializeField] private AudioClip[] musics;
-
     [SerializeField] private AudioSource controlAudio;
+    [SerializeField] AudioMixer mixer;
 
 
-    private void Start()
-    {
-        controlAudio = GetComponent<AudioSource>();
-        audioManager.PlayMusic(GameManager.instance.ActiveScene, GameManager.instance.MusicVolume());
-        
-    }
+    const string MASTER_VOLUME = "MasterVolume";
+    const string MUSIC_VOLUME = "MusicVolume";
+    const string SFX_VOLUME = "SfxVolume";
     private void Awake()
     {
 
@@ -33,12 +30,28 @@ public class AudioManager : MonoBehaviour
             DontDestroyOnLoad(this);
         }
     }
+    private void Start()
+    {
+        controlAudio = GetComponent<AudioSource>();
+        audioManager.PlayMusic(GameManager.instance.ActiveScene, GameManager.instance.MusicVolume());
+    }
     public void PlayMusic(int indice, float volumen)
     {
         controlAudio.PlayOneShot(musics[indice], volumen);
         controlAudio.loop = true;
     }
-
+    public void SetMasterVolume(float value)
+    {
+        mixer.SetFloat(MASTER_VOLUME, Mathf.Log10(value) * 20);
+    }
+    public void SetMusicVolume(float value)
+    {
+        mixer.SetFloat(MUSIC_VOLUME, Mathf.Log10(value) * 20);
+    }
+    void SetSfxVolume(float value)
+    {
+        mixer.SetFloat(SFX_VOLUME, Mathf.Log10(value) * 20);
+    }
     public void StopMusic ()
     {
         controlAudio.Stop();
